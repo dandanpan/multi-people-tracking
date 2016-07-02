1aa0974dc3d5ad1078768e3939896eb809a9fe1e
ccceeefff <cef.ramirez@gmail.com>
2016-07-01 21:11:41 -0700

    adding interface parameters

--- scripts/heartbeat/heartbeat.js
+++ scripts/heartbeat/heartbeat.js
@@ -1,4 +1,5 @@
 var firebase = require('firebase');
+var macaddress = require('macaddress');
 
 var firebaseConfig = {
     databaseURL: 'https://footstep-wsn-prod.firebaseio.com/'
@@ -9,10 +10,14 @@ firebase.initializeApp(firebaseConfig);
 var count = 0;
 
 setInterval(function(){
+  var iface = macaddress.networkInterfaces()["en0"];
   firebase.database().ref('testData/test1').set({
     'name': 'shijia',
-    'data' : count++
+    'data' : count++,
+    'mac': iface["mac"],
+    'localip': iface["ipv4"]
   }).then(function(){
+    // TODO: fix logging!!
     console.log("done publishing");
   }).catch(function(){
     console.log("failed to publish");
